185b61437e4e9fe5b006b2e43bfea2b5066a8fc5
yituodab <a111664@qq.com>
2024-05-10 17:53:51 +0800

    1

--- mc.py
+++ mc.py
@@ -3,26 +3,38 @@ import os
 minecraft_directory = ".minecraft"
 if not os.path.exists(r"C:\Program Files\wangzhimeng"):
 	os.mkdir(r"C:\Program Files\wangzhimeng")
-if not os.path.exists(r"C:\Program Files\wangzhimeng\option"):
-	op = open(r"C:\Program Files\wangzhimeng\option", "a")
-	op.write("#This is mc options")
-	op.close()
 option_dir = r"C:\Program Files\wangzhimeng"
-option_file = r"C:\Program Files\wangzhimeng\option"
 print('Warning：本启动器为往之门服务器定制，且仅能使用离线用户')
-file = open(option_file, "a")
-lines = file.readlines()
-for line in lines:
-	if line.find("install=true") == -1:
-		Input = input("是否安装？（是/否）")
-		if Input == '否' :
-			exit()
-		if Input == '是' :
-			Dir = input("(默认安装目录：.minecraft)\n安装目录？(按回车选择默认：")
-			minecraft_launcher_lib.install.install_minecraft_version("1.18.2", minecraft_directory)
-			file.write("install=true\n")
-
-file.close()
+def install():
+	Dir = input("(默认安装目录：.minecraft)\n安装目录？(按回车选择默认)：")
+	if Dir == '':
+		print('安装中...')
+		minecraft_launcher_lib.install.install_minecraft_version("1.18.2", minecraft_directory)
+		print('安装forge中...')
+		minecraft_launcher_lib.forge.install_forge_version("40.2.0", minecraft_directory)
+		file = open(option_dir+r"install")
+		file.write(minecraft_directory)
+		file.close()
+	else:
+		print('安装中...')
+		minecraft_launcher_lib.install.install_minecraft_version("1.18.2", Dir)
+		print('安装forge中...')
+		minecraft_launcher_lib.forge.install_forge_version("40.2.0", Dir)
+		file = open(option_dir+r"install", "a+")
+		file.write(Dir)
+		file.close()
+if os.path.exists(option_dir+r"install"):
+	print('已安装！')
+else:
+	Input = input("是否安装？（是/否）")
+	if Input == '否':
+		exit()
+	if Input == '是':
+		install()
+with open(option_dir+r"\install", "r") as file:
+	# 读取文件内容
+	content = file.read()
+print('安装目录：')
 """
 minecraft_directory = ".minecraft"
 minecraft_launcher_lib.install.install_minecraft_version("1.18.2", minecraft_directory)
